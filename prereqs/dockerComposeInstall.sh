87b70666ef34dd894dec22ec51a7cb24baf74469
Throbac Labs <93233216+Throbac-Labs@users.noreply.github.com>
2022-06-24 11:13:25 -0500

    Create dockerComposeInstall.sh

---
+++ prereqs/dockerComposeInstall.sh
@@ -0,0 +1,5 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo chgrp docker /usr/local/bin/docker-compose
+sudo chmod 750 /usr/local/bin/docker-compose
